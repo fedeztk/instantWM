68df2a09bcc37df106d6e03e78ef93cf49a46ff7
Dušan Uverić <dusan.uveric9@gmail.com>
2021-05-03 00:55:33 +0200

    slow down event spamming in main loop
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- instantwm.c
+++ instantwm.c
@@ -4027,7 +4027,7 @@ run(void)
 	/* main event loop */
 	XSync(dpy, False);
 	while (running && !XNextEvent(dpy, &ev))
-		if (handler[ev.type])
+		if (handler[ev.type] && XEventsQueued(dpy, QueuedAlready) < 250)
 			handler[ev.type](&ev); /* call handler */
 }
 
