41887ebfe0d7a32b184882ad093675d60ed38c95
paperbenni <paperbenni@gmail.com>
2021-05-05 01:33:55 +0200

    speed up resizing by moving smartborder logic
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>

--- push.c
+++ push.c
@@ -18,12 +18,18 @@ prevc(Client *c, float f) {
 }
 
 // get amount of tiled clients
-int clientcount()
+
+int clientcountmon(Monitor *m)
 {
 	int n;
 	Client *c;
-	for (n = 0, c = nexttiled(selmon->clients); c; c = nexttiled(c->next), n++);
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
 	return n;
+}
+
+int clientcount()
+{
+	return clientcountmon(selmon);
 }
 
 int allclientcount()
